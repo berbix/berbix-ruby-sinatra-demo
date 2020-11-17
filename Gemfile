8cf0c302f6299fd3ab7be337b561552405ddd460
Jeff Yip <jeffyip@users.noreply.github.com>
2020-11-16 18:24:52 -0600

    Use refresh tokens

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org'
 gem 'sinatra', '2.0.8.1'
 gem 'sinatra-contrib', '2.0.8.1'
 
-gem 'berbix', '0.0.6'
+gem 'berbix', '0.0.10'
+
 gem 'namey', '>= 0.0.12'
--- app.rb
+++ app.rb
@@ -3,27 +3,51 @@ require 'sinatra/reloader' if development?
 require 'yaml'
 require 'berbix'
 require 'namey'
+require 'json'
+require 'sinatra/custom_logger'
+require 'logger'
+
+set :logger, Logger.new(STDOUT)
 
 berbix_config = YAML.load(File.read('berbix_config.yaml'))
 
 get '/' do
   @generator = Namey::Generator.new
   @username = @generator.name(:common)
-  @uid = rand(10000000000)
+  
+  if params[:uid]
+    @uid = params[:uid]
+  else
+    @uid = 123
+  end
 
   client = Berbix::Client.new(
     client_secret: berbix_config['client_secret'],
     environment: :production
   )
 
-  @transaction_tokens = client.create_transaction(
-    customer_uid: @uid, # ID for the user in client database
-    template_key: berbix_config['template_key'], # Template key for this transaction
-  )
-
-  # To avoid dealing with a DB for this demo, we'll write the refresh token to a file
-  File.open("refresh_token.txt", "w") do |file|
-    file.write(@transaction_tokens.refresh_token)
+  # Check if refresh token exists for given UID
+  if File.file?("#{@uid}_refresh_token.txt")
+    # read the refresh token from disk
+    @refresh_token = File.open("#{@uid}_refresh_token.txt").read
+    
+    begin
+      @transaction_tokens = Berbix::Tokens.from_refresh(@refresh_token)
+      @data = client.fetch_transaction(@transaction_tokens) # needed to force library to refresh the refresh token
+    rescue => exception
+      @transaction_tokens = nil
+    end
+  end    
+    
+  if @transaction_tokens.nil?
+    @transaction_tokens = client.create_transaction(
+      customer_uid: @uid, # ID for the user in client database
+      template_key: berbix_config['template_key'], # Template key for this transaction
+      hosted_options: {}
+    )
+    File.open("#{@uid}_refresh_token.txt", "w") do |file|
+      file.write(@transaction_tokens.refresh_token)
+    end
   end
 
   erb :index
@@ -38,7 +62,7 @@ get '/after_id_check' do
   )
 
   transaction_tokens = Berbix::Tokens.from_refresh(@refresh_token)
-  
+
   @data = client.fetch_transaction(transaction_tokens)
 
   @name_on_id = @data["fields"]["given_name"]["value"] + " " + @data["fields"]["family_name"]["value"]
