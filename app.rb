a704acd4dcdb400369af08712ce82892aaf95eea
Jeff Yip <jeffyip@users.noreply.github.com>
2021-03-18 18:39:41 -0700

    Skip refresh_token and hosted_options

--- app.rb
+++ app.rb
@@ -23,18 +23,17 @@ get '/' do
   )
 
   # Check if refresh token exists for given UID
-  if File.file?("#{@uid.to_i}_refresh_token.txt")
-    # read the refresh token from disk
-    @refresh_token = File.open("#{@uid.to_i}_refresh_token.txt").read
-    @transaction_tokens = Berbix::Tokens.from_refresh(@refresh_token)
-    @transaction_tokens = client.refresh_tokens(@transaction_tokens) # needed to force library to refresh the refresh token
-  end
+  # if File.file?("#{@uid.to_i}_refresh_token.txt")
+  #   # read the refresh token from disk
+  #   @refresh_token = File.open("#{@uid.to_i}_refresh_token.txt").read
+  #   @transaction_tokens = Berbix::Tokens.from_refresh(@refresh_token)
+  #   @transaction_tokens = client.refresh_tokens(@transaction_tokens) # needed to force library to refresh the refresh token
+  # end
 
   if @transaction_tokens.nil?
     @transaction_tokens = client.create_transaction(
       customer_uid: @uid, # ID for the user in client database
       template_key: berbix_config['template_key'], # Template key for this transaction
-      hosted_options: {}
     )
     File.open("#{@uid.to_i}_refresh_token.txt", "w") do |file|
       file.write(@transaction_tokens.refresh_token)
