8e9d63a1e64722277f78d8e4d2991fbbded05302
Jeff Yip <jeffyip@users.noreply.github.com>
2021-03-18 19:28:38 -0700

    fixup

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ if File.file?('berbix_config.yaml')
   berbix_config = YAML.load(File.read('berbix_config.yaml'))
 else
   berbix_config = {
-    'template_key' => process.env.BERBIX_TEMPLATE,
-    'client_secret' => process.env.BERBIX_SECRET
+    'template_key' => ENV['BERBIX_TEMPLATE'],
+    'client_secret' => ENV['BERBIX_SECRET']
   }
 end
 
